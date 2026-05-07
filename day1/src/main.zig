898ca7d1b08279de696e43370e1dffec6429e69c
jimitiki <dev@jimitiki.com>
2026-05-07 11:39:00 -0400

    Read input from file

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -2,29 +2,29 @@ const std = @import("std");
 const Io = std.Io;
 const assert = std.debug.assert;
 
-const inputs = [_][]const u8{
-    "L68",
-    "L30",
-    "R48",
-    "L5",
-    "R60",
-    "L55",
-    "L1",
-    "L99",
-    "R14",
-    "L82",
-};
-
 var dial: i32 = 50;
 
 pub fn main(init: std.process.Init) !void {
+    const arena: std.mem.Allocator = init.arena.allocator();
+
     const io = init.io;
-    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_buffer: [256]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout = &stdout_file_writer.interface;
 
+    const input_file = try std.Io.Dir.cwd().openFile(io, "data/test.txt", .{});
+    defer input_file.close(io);
+
+    var read_buffer: [256]u8 = undefined;
+    var reader = input_file.reader(io, &read_buffer);
+    var inputs = try std.ArrayList([]const u8).initCapacity(arena, 128);
+    defer inputs.deinit(arena);
+    while (try reader.interface.takeDelimiter('\n')) |line| {
+        try inputs.append(arena, line);
+    }
+
     var zeroes: u32 = 0;
-    for (inputs) |input| {
+    for (inputs.items) |input| {
         const dir = input[0];
         const mag = try std.fmt.parseInt(u16, input[1..input.len], 10);
         if (dir == 'L') {

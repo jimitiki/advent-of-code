a5c465a08ea1bb5322acf388b5967f2a7213c842
jimitiki <dev@jimitiki.com>
2026-05-07 18:19:10 -0400

    Implement day 1 part 2

--- day1/src/main.zig
+++ day1/src/main.zig
@@ -6,14 +6,16 @@ pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
 
     const args = try init.minimal.args.toSlice(arena);
-    assert(args.len > 1);
+    assert(args.len > 2);
+    assert(args[1].len == 2);
+    assert(args[1][0] == 'p');
 
     const io = init.io;
     var stdout_buffer: [256]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout = &stdout_file_writer.interface;
 
-    const file_path = try std.fmt.allocPrint(arena, "data/{s}.txt", .{args[1]});
+    const file_path = try std.fmt.allocPrint(arena, "data/{s}.txt", .{args[2]});
     const input_file = try std.Io.Dir.cwd().openFile(io, file_path, .{});
     defer input_file.close(io);
 
@@ -22,14 +24,20 @@ pub fn main(init: std.process.Init) !void {
     var inputs = try std.ArrayList([]const u8).initCapacity(arena, 128);
     defer inputs.deinit(arena);
 
+    const algo: *const fn (u8, i32) u8 = switch (args[1][1]) {
+        '1' => test_line_p1,
+        '2' => test_line_p2,
+        else => unreachable,
+    };
+
     var dial: u8 = 50;
     var pwd: u32 = 0;
     while (try reader.interface.takeDelimiter('\n')) |line| {
         const dir = line[0];
         if (std.fmt.parseInt(u16, line[1..line.len], 10)) |mag| {
             const change: i32 = if (dir == 'L') -@as(i32, mag) else if (dir == 'R') mag else unreachable;
-            pwd += test_line_p1(dial, change);
-            dial = @intCast(@mod(dial + change, 100));
+            pwd += algo(dial, change);
+            dial = @intCast(@mod(@as(i32, dial) + change, 100));
         } else |_| {
             try stdout.print("Error in line: {s}\n", .{line});
             try stdout.flush();
@@ -43,4 +51,18 @@ pub fn main(init: std.process.Init) !void {
 
 fn test_line_p1(pos: u8, change: i32) u8 {
     return if (@mod(pos + change, 100) == 0) 1 else 0;
+}
+
+fn test_line_p2(pos: u8, change: i32) u8 {
+    if (change > 0) {
+        return @intCast(@divFloor(change + pos, 100));
+    } else {
+        const full_turns: u8 = @intCast(@divFloor(@abs(change), 100));
+        const extra: u8 = @intCast(100 - @mod(change, 100));
+        if (pos != 0 and extra >= pos) {
+            return full_turns + 1;
+        } else {
+            return full_turns;
+        }
+    }
 }

3f037fd1f9256ff74ff36f53b02f8fc9084f499b
jimitiki <dev@jimitiki.com>
2026-05-10 12:32:33 -0400

    Generalize day 3 logic

--- day3/src/main.zig
+++ day3/src/main.zig
@@ -2,34 +2,39 @@ const std = @import("std");
 
 const Init = @import("lib").Init;
 
+const digits = "987654321";
+
 pub fn main(init: std.process.Init) !void {
-    var stdout_buffer: [256]u8 = undefined;
-    var read_buffer: [256]u8 = undefined;
+    var stdout_buffer: [64]u8 = undefined;
+    var read_buffer: [128]u8 = undefined;
     var ini = try Init.init(init, &stdout_buffer, &read_buffer);
     defer ini.deinit();
 
     var stdout = &ini.stdout_writer.interface;
     var input = &ini.input_reader.interface;
-    var answer: u32 = 0;
+    var answer: u64 = 0;
     while (try input.takeDelimiter('\n')) |line| {
-        answer += highestJoltage(line);
+        answer += try highestJoltage(line, 2);
     }
 
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
 }
 
-fn highestJoltage(bank: []u8) u8 {
-    var tens: u8 = '9';
-    while (tens >= '1') : (tens -= 1) {
-        const tidx = if (findBattery(bank, tens)) |idx| idx else continue;
-        var ones: u8 = '9';
-        while (ones >= '1') : (ones -= 1) {
-            if (findBattery(bank[tidx + 1 ..], ones)) |_| {} else continue;
-            return digitsToInt(tens, ones);
+fn highestJoltage(bank: []u8, battery_cnt: u8) !u64 {
+    var digit_buf: [128]u8 = undefined;
+    var min_idx: usize = 0;
+    for (0..battery_cnt) |jolt_idx| {
+        for (digits) |digit| {
+            const subbank = bank[min_idx .. bank.len - (battery_cnt - jolt_idx) + 1];
+            if (findBattery(subbank, digit)) |battery_idx| {
+                digit_buf[jolt_idx] = digit;
+                min_idx = min_idx + battery_idx + 1;
+                break;
+            }
         }
     }
-    return 0;
+    return try std.fmt.parseInt(u64, digit_buf[0..battery_cnt], 10);
 }
 
 fn findBattery(bank: []u8, joltage: u8) ?usize {
@@ -37,8 +42,4 @@ fn findBattery(bank: []u8, joltage: u8) ?usize {
         if (battery == joltage) return i;
     }
     return null;
-}
-
-fn digitsToInt(digit1: u8, digit2: u8) u8 {
-    return (digit1 - 48) * 10 + digit2 - 48;
 }

ee04edfd0f66e69ce40f506e1552f988352133b4
jimitiki <dev@jimitiki.com>
2026-05-11 09:25:42 -0400

    Read day 5 input

--- day5/src/main.zig
+++ day5/src/main.zig
@@ -9,6 +9,25 @@ pub fn main(init: std.process.Init) !void {
     defer ini.deinit();
 
     var stdout = &ini.stdout_writer.interface;
+    var input = &ini.input_reader.interface;
+
+    //Handle ID ranges
+    while (try input.takeDelimiter('\n')) |line| {
+        if (line.len == 0) break;
+        var split_point: usize = 0;
+        while (line[split_point] != '-') : (split_point += 1) {}
+        const start = try std.fmt.parseInt(u64, line[0..split_point], 10);
+        const end = try std.fmt.parseInt(u64, line[split_point + 1 ..], 10);
+        _ = start;
+        _ = end;
+    }
+
+    // Handle IDs
+    while (try input.takeDelimiter('\n')) |line| {
+        const id = try std.fmt.parseInt(u64, line, 10);
+        _ = id;
+    }
+
     const answer = "";
 
     try stdout.print("{s}\n", .{answer});

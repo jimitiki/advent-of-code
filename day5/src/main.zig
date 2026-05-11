b4f3fcc3c129062dae35da9a324971a408228358
jimitiki <dev@jimitiki.com>
2026-05-11 09:41:17 -0400

    Implement day 5 part 1

--- day5/src/main.zig
+++ day5/src/main.zig
@@ -2,6 +2,15 @@ const std = @import("std");
 
 const Init = @import("lib").Init;
 
+const Range = struct {
+    start: u64,
+    end: u64,
+
+    pub fn lessThan(_: void, lhs: Range, rhs: Range) bool {
+        return lhs.start < rhs.start;
+    }
+};
+
 pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [256]u8 = undefined;
     var read_buffer: [256]u8 = undefined;
@@ -11,25 +20,28 @@ pub fn main(init: std.process.Init) !void {
     var stdout = &ini.stdout_writer.interface;
     var input = &ini.input_reader.interface;
 
-    //Handle ID ranges
+    //Read ID ranges from file
+    var ranges: std.ArrayList(Range) = .empty;
     while (try input.takeDelimiter('\n')) |line| {
         if (line.len == 0) break;
         var split_point: usize = 0;
         while (line[split_point] != '-') : (split_point += 1) {}
         const start = try std.fmt.parseInt(u64, line[0..split_point], 10);
         const end = try std.fmt.parseInt(u64, line[split_point + 1 ..], 10);
-        _ = start;
-        _ = end;
+        try ranges.append(ini.arena, .{ .start = start, .end = end });
     }
 
-    // Handle IDs
+    var answer: u32 = 0;
     while (try input.takeDelimiter('\n')) |line| {
         const id = try std.fmt.parseInt(u64, line, 10);
-        _ = id;
+        for (ranges.items) |range| {
+            if (id >= range.start and id <= range.end) {
+                answer += 1;
+                break;
+            }
+        }
     }
 
-    const answer = "";
-
-    try stdout.print("{s}\n", .{answer});
+    try stdout.print("{}\n", .{answer});
     try stdout.flush();
 }

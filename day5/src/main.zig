74c3ffac8f4cdeded4f93c810597aa341d800a8f
jimitiki <dev@jimitiki.com>
2026-05-11 09:58:09 -0400

    Sort and merge input ranges

--- day5/src/main.zig
+++ day5/src/main.zig
@@ -21,23 +21,38 @@ pub fn main(init: std.process.Init) !void {
     var input = &ini.input_reader.interface;
 
     //Read ID ranges from file
-    var ranges: std.ArrayList(Range) = .empty;
+    var ranges_raw: std.ArrayList(Range) = .empty;
     while (try input.takeDelimiter('\n')) |line| {
         if (line.len == 0) break;
         var split_point: usize = 0;
         while (line[split_point] != '-') : (split_point += 1) {}
         const start = try std.fmt.parseInt(u64, line[0..split_point], 10);
         const end = try std.fmt.parseInt(u64, line[split_point + 1 ..], 10);
-        try ranges.append(ini.arena, .{ .start = start, .end = end });
+        try ranges_raw.append(ini.arena, .{ .start = start, .end = end });
     }
 
+    // Merge overlapping ranges
+    std.sort.pdq(Range, ranges_raw.items, {}, Range.lessThan);
+    var ranges: std.ArrayList(Range) = .empty;
+    var range_merged: Range = .{ .start = ranges_raw.items[0].start, .end = ranges_raw.items[0].end };
+    for (ranges_raw.items) |range| {
+        if (range.start > range_merged.end) {
+            try ranges.append(ini.arena, range_merged);
+            range_merged = range;
+        } else {
+            range_merged.end = @max(range.end, range_merged.end);
+        }
+    } else {
+        try ranges.append(ini.arena, range_merged);
+    }
+
+    // Handle IDs
     var answer: u32 = 0;
     while (try input.takeDelimiter('\n')) |line| {
         const id = try std.fmt.parseInt(u64, line, 10);
         for (ranges.items) |range| {
             if (id >= range.start and id <= range.end) {
                 answer += 1;
-                break;
             }
         }
     }

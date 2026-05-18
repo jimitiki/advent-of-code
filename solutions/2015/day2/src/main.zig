09cb557626fd9dccf11810a2c7eae41bb6784bb3
jimitiki <dev@jimitiki.com>
2026-05-18 07:27:42 -0400

    2015.2: Implement part 1

--- solutions/2015/day2/src/main.zig
+++ solutions/2015/day2/src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 
 const Boilerplate = @import("boilerplate").Boilerplate;
 
+const Box = struct { u32, u32, u32 };
+
 pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [256]u8 = undefined;
     var read_buffer: [256]u8 = undefined;
@@ -10,9 +12,28 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
+    var answer: u32 = 0;
     while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+        const wstart = for (line, 0..) |c, i| {
+            if (c == 'x') {
+                break i + 1;
+            }
+        } else return error.InvalidInput;
+        const hstart = for (line[wstart + 1 ..], wstart + 1..) |c, i| {
+            if (c == 'x') {
+                break i + 1;
+            }
+        } else return error.InvalidInput;
+        const box: Box = .{
+            try std.fmt.parseUnsigned(u32, line[0 .. wstart - 1], 10),
+            try std.fmt.parseUnsigned(u32, line[wstart .. hstart - 1], 10),
+            try std.fmt.parseUnsigned(u32, line[hstart..], 10),
+        };
+        const area_lw = box[0] * box[1];
+        const area_lh = box[0] * box[2];
+        const area_wh = box[1] * box[2];
+        const surface_area = area_lw * 2 + area_lh * 2 + area_wh * 2;
+        answer += surface_area + @min(box[0] * box[1], box[0] * box[2], box[1] * box[2]);
     }
 
     try stdout.print("{}\n", .{answer});

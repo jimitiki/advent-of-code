a2a2baeb43fb7c3baf08ebc2fb2ca6497005df1f
jimitiki <dev@jimitiki.com>
2026-05-17 21:56:27 -0400

    2015.1: Implement part 1

--- solutions/2015/day1/src/main.zig
+++ solutions/2015/day1/src/main.zig
@@ -4,15 +4,21 @@ const Boilerplate = @import("boilerplate").Boilerplate;
 
 pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [256]u8 = undefined;
-    var read_buffer: [256]u8 = undefined;
+    var read_buffer: [1]u8 = undefined;
     var bp = try Boilerplate.init(init, &stdout_buffer, &read_buffer);
     defer bp.deinit();
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
-    while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+    var answer: i32 = 0;
+    while (true) {
+        if (input.takeByte()) |c| {
+            switch (c) {
+                '(' => answer += 1,
+                ')' => answer -= 1,
+                else => break,
+            }
+        } else |_| break;
     }
 
     try stdout.print("{}\n", .{answer});

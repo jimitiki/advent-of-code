2b9fcf76fcd539fde6ff2160b03fdd3b6e9c3615
jimitiki <dev@jimitiki.com>
2026-05-17 22:01:10 -0400

    2015.1: Implement part 2

--- solutions/2015/day1/src/main.zig
+++ solutions/2015/day1/src/main.zig
@@ -10,17 +10,23 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    var answer: i32 = 0;
-    while (true) {
+    var floor: i32 = 0;
+    var pos: i32 = 1;
+    while (true) : (pos += 1) {
         if (input.takeByte()) |c| {
             switch (c) {
-                '(' => answer += 1,
-                ')' => answer -= 1,
+                '(' => floor += 1,
+                ')' => floor -= 1,
                 else => break,
             }
+            if (bp.part == .p2 and floor < 0) break;
         } else |_| break;
     }
 
+    const answer = switch (bp.part) {
+        .p1 => floor,
+        .p2 => pos,
+    };
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
 }

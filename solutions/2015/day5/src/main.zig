6b388648c470b91f30a94d9f3df267e3342dcf40
jimitiki <dev@jimitiki.com>
2026-05-18 21:53:29 -0400

    2015.5: Implement part 1

--- solutions/2015/day5/src/main.zig
+++ solutions/2015/day5/src/main.zig
@@ -10,11 +10,46 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
+    var answer: usize = 0;
     while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+        var vowels: usize = 0;
+        var double: bool = false;
+        if (isVowel(line[0])) {
+            vowels += 1;
+        }
+        const contains_bad = for (line[0 .. line.len - 1], line[1..]) |a, b| {
+            if (isVowel(b)) {
+                vowels += 1;
+            }
+            if (a == b) {
+                double = true;
+            }
+            if (isForbiddenPair(a, b)) {
+                break true;
+            }
+        } else false;
+        if (vowels >= 3 and double and !contains_bad) {
+            answer += 1;
+        }
     }
 
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
+}
+
+fn isVowel(char: u8) bool {
+    return switch (char) {
+        'a', 'e', 'i', 'o', 'u' => true,
+        else => false,
+    };
+}
+
+fn isForbiddenPair(a: u8, b: u8) bool {
+    return switch (a) {
+        'a' => b == 'b',
+        'c' => b == 'd',
+        'p' => b == 'q',
+        'x' => b == 'y',
+        else => false,
+    };
 }

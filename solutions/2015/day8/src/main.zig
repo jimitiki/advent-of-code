ebba60ad1459e62ee0430194cd49c7778e031d48
jimitiki <dev@jimitiki.com>
2026-05-19 23:56:00 -0400

    2015.8: Implement part 1

--- solutions/2015/day8/src/main.zig
+++ solutions/2015/day8/src/main.zig
@@ -10,11 +10,37 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
+    var answer: u64 = 0;
     while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+        var i: usize = 1;
+        var mem_chars: usize = 0;
+        if (line[0] != '"' or line[line.len - 1] != '"') {
+            return error.InvalidInput;
+        }
+        while (i < line.len - 1) : (i += 1) {
+            mem_chars += 1;
+            if (line[i] == '\\') {
+                i += 1;
+                switch (line[i]) {
+                    '"', '\\' => {},
+                    'x' => {
+                        if (!isHex(line[i + 1]) or !isHex(line[i + 2])) {
+                            return error.InvalidInput;
+                        } else {
+                            i += 2;
+                        }
+                    },
+                    else => return error.InvalidInput,
+                }
+            }
+        }
+        answer += line.len - mem_chars;
     }
 
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
+}
+
+fn isHex(char: u8) bool {
+    return char >= '0' and char <= '9' or char >= 'A' and char <= 'Z' or char >= 'a' and char <= 'z';
 }

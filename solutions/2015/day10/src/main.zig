5672df63c3473dd66094649155f243858926af5a
jimitiki <dev@jimitiki.com>
2026-05-21 08:04:07 -0400

    2015.10: Implement part 1

--- solutions/2015/day10/src/main.zig
+++ solutions/2015/day10/src/main.zig
@@ -1,20 +1,46 @@
 const std = @import("std");
 
-const Boilerplate = @import("lib").Boilerplate;
-
 pub fn main(init: std.process.Init) !void {
-    var stdout_buffer: [256]u8 = undefined;
-    var read_buffer: [256]u8 = undefined;
-    var bp = try Boilerplate.init(init, &stdout_buffer, &read_buffer);
-    defer bp.deinit();
+    const arena = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(arena);
+    defer arena.free(args);
+    const seed = args[2];
+    const iterations = try std.fmt.parseUnsigned(usize, args[3], 10);
 
-    var stdout = &bp.stdout_writer.interface;
-    var input = &bp.input_reader.interface;
-    const answer = 0;
-    while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+    var stdout_buffer: [256]u8 = undefined;
+    var stdout_writer: std.Io.File.Writer = .init(.stdout(), init.io, &stdout_buffer);
+    var stdout = &stdout_writer.interface;
+
+    var buf_cur: []u8 = try arena.alloc(u8, seed.len);
+    defer arena.free(buf_cur);
+    for (seed, 0..) |char, i| {
+        buf_cur[i] = char - 48;
+    }
+    var sequence: []const u8 = buf_cur;
+    for (0..iterations) |_| {
+        const buf_next: []u8 = try arena.alloc(u8, sequence.len * 2);
+        const next_sequence: []const u8 = lookSay(sequence, buf_next);
+        arena.free(buf_cur);
+        buf_cur = buf_next;
+        sequence = next_sequence;
     }
 
-    try stdout.print("{}\n", .{answer});
+    try stdout.print("{}\n", .{sequence.len});
     try stdout.flush();
+}
+
+fn lookSay(sequence: []const u8, buf: []u8) []const u8 {
+    var i: usize = 0;
+    var j: usize = 0;
+    while (i < sequence.len) : (i += 1) {
+        const digit = sequence[i];
+        var count: u8 = 1;
+        while (i + count < sequence.len and sequence[i + count] == digit) : (count += 1) {}
+        buf[j] = count;
+        j += 1;
+        buf[j] = digit;
+        j += 1;
+        i += count - 1;
+    }
+    return buf[0..j];
 }

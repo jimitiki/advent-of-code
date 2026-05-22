a034a15a8b5937be6ffc36fdb22f8514dceda2d5
jimitiki <dev@jimitiki.com>
2026-05-22 11:25:05 -0400

    2015.15: Implement part 1

--- lib/parse.zig
+++ lib/parse.zig
@@ -5,20 +5,43 @@ pub const WordIterator = struct {
 
     string: []const u8,
     index: usize = 0,
+    omit_punctuation: bool = false,
 
     pub fn init(string: []const u8) Self {
         return .{ .string = string };
     }
 
     pub fn next(self: *Self) ?[]const u8 {
-        while (self.index < self.string.len and self.string[self.index] == ' ') : (self.index += 1) {}
+        while (self.skip()) : (self.index += 1) {}
         if (self.index >= self.string.len) {
             return null;
         }
 
         const start = self.index;
-        while (self.index < self.string.len and self.string[self.index] != ' ') : (self.index += 1) {}
+        while (self.take()) : (self.index += 1) {}
         return self.string[start..self.index];
+    }
+
+    fn skip(self: *Self) bool {
+        if (self.index >= self.string.len) {
+            return false;
+        }
+        return switch (self.string[self.index]) {
+            ' ' => true,
+            ',', '.' => self.omit_punctuation,
+            else => false,
+        };
+    }
+
+    fn take(self: *Self) bool {
+        if (self.index >= self.string.len) {
+            return false;
+        }
+        return switch (self.string[self.index]) {
+            ' ' => false,
+            ',', '.' => !self.omit_punctuation,
+            else => true,
+        };
     }
 };
 
--- solutions/2015/day15/src/main.zig
+++ solutions/2015/day15/src/main.zig
@@ -3,6 +3,14 @@ const std = @import("std");
 const lib = @import("lib");
 const Boilerplate = lib.Boilerplate;
 
+const Ingredient = struct {
+    calories: i64,
+    capacity: i64,
+    durability: i64,
+    flavor: i64,
+    texture: i64,
+};
+
 pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [256]u8 = undefined;
     var read_buffer: [256]u8 = undefined;
@@ -11,11 +19,57 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
+    var ingredients: std.ArrayList(Ingredient) = .empty;
     while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+        try ingredients.append(bp.arena, try parseIngredient(line));
     }
 
-    try stdout.print("{}\n", .{answer});
+    const quantities = try bp.arena.alloc(u8, ingredients.items.len);
+    defer bp.arena.free(quantities);
+    try stdout.print("{}\n", .{maxScore(ingredients.items, quantities, 0, 0)});
     try stdout.flush();
+}
+
+fn parseIngredient(string: []const u8) !Ingredient {
+    var it: lib.parse.WordIterator = .{ .string = string, .omit_punctuation = true };
+    _ = it.next();
+    return .{
+        .capacity = try parseProperty(&it),
+        .durability = try parseProperty(&it),
+        .flavor = try parseProperty(&it),
+        .texture = try parseProperty(&it),
+        .calories = try parseProperty(&it),
+    };
+}
+
+fn parseProperty(it: *lib.parse.WordIterator) !i64 {
+    _ = it.next();
+    return try std.fmt.parseInt(i64, it.next().?, 10);
+}
+
+fn maxScore(ingredients: []const Ingredient, quantities: []u8, count: usize, amount_used: usize) i64 {
+    if (count == ingredients.len - 1) {
+        quantities[count] = @truncate(100 - amount_used);
+        return computeScore(ingredients, quantities);
+    }
+    var max: i64 = 0;
+    for (0..100 - amount_used) |amount| {
+        quantities[count] = @truncate(amount);
+        max = @max(max, maxScore(ingredients, quantities, count + 1, amount_used + amount));
+    }
+    return max;
+}
+
+fn computeScore(ingredients: []const Ingredient, quantities: []const u8) i64 {
+    var capacity: i64 = 0;
+    var durability: i64 = 0;
+    var flavor: i64 = 0;
+    var texture: i64 = 0;
+    for (ingredients, quantities) |ingredient, quantity| {
+        capacity += quantity * ingredient.capacity;
+        durability += quantity * ingredient.durability;
+        flavor += quantity * ingredient.flavor;
+        texture += quantity * ingredient.texture;
+    }
+    return @max(0, capacity) * @max(0, durability) * @max(0, flavor) * @max(0, texture);
 }

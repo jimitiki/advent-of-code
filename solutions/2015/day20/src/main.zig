abfb2dd34ff9c7cdd89bcb07292f8d62e0dd9b51
jimitiki <dev@jimitiki.com>
2026-05-23 12:27:30 -0400

    2015.20: Implement part 1

--- solutions/2015/day20/src/main.zig
+++ solutions/2015/day20/src/main.zig
@@ -4,18 +4,37 @@ const lib = @import("lib");
 const Boilerplate = lib.Boilerplate;
 
 pub fn main(init: std.process.Init) !void {
-    var stdout_buffer: [256]u8 = undefined;
-    var read_buffer: [256]u8 = undefined;
-    var bp = try Boilerplate.init(init, &stdout_buffer, &read_buffer);
-    defer bp.deinit();
+    const arena = init.arena.allocator();
+    const args = try init.minimal.args.toSlice(arena);
+    defer arena.free(args);
+    const target = try std.fmt.parseUnsigned(u32, args[3], 10);
 
-    var stdout = &bp.stdout_writer.interface;
-    var input = &bp.input_reader.interface;
-    const answer = 0;
-    while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+    var house: u32 = 1;
+    while (sumFactors(house) * 10 < target) : (house += 1) {}
+
+    var stdout_buffer: [256]u8 = undefined;
+    var stdout_writer: std.Io.File.Writer = .init(.stdout(), init.io, &stdout_buffer);
+    var stdout = &stdout_writer.interface;
+    try stdout.print("{}\n", .{house});
+    try stdout.flush();
+}
+
+fn sumFactors(number: u32) u32 {
+    if (number == 0) {
+        return 0;
+    } else if (number == 1) {
+        return 1;
     }
 
-    try stdout.print("{}\n", .{answer});
-    try stdout.flush();
+    var sum: u32 = 1 + number;
+    const cutoff = @sqrt(@as(f64, @floatFromInt(number)));
+    var factor: u32 = 2;
+    while (factor <= cutoff) : (factor += 1) {
+        if (number % factor == 0) {
+            sum += factor;
+            const other = number / factor;
+            if (factor != other) sum += other;
+        }
+    }
+    return sum;
 }

e6f1beda21b729efeea92c5e6a638b64efba518b
jimitiki <dev@jimitiki.com>
2026-05-22 23:11:01 -0400

    2015.17: Implement part 2

--- solutions/2015/day17/src/main.zig
+++ solutions/2015/day17/src/main.zig
@@ -19,12 +19,20 @@ pub fn main(init: std.process.Init) !void {
         try containers.append(bp.arena, try std.fmt.parseUnsigned(u32, line, 10));
     }
 
-    try stdout.print("{}\n", .{countCombinations(containers.items, 0)});
+    if (bp.part == .p1) {
+        try stdout.print("{}\n", .{countCombinations(containers.items, 0, 0, 0)});
+    } else {
+        const limit = minUsed(containers.items, 0, 0);
+        try stdout.print("{}\n", .{countCombinations(containers.items, 0, 0, limit)});
+    }
+
     try stdout.flush();
 }
 
-fn countCombinations(containers: []u32, capacity: u32) u32 {
-    if (capacity == 150) {
+fn countCombinations(containers: []u32, capacity: u32, used: u32, limit: u32) u32 {
+    if (limit > 0 and used > limit) {
+        return 0;
+    } else if (capacity == 150) {
         return 1;
     } else if (capacity > 150) {
         return 0;
@@ -34,7 +42,23 @@ fn countCombinations(containers: []u32, capacity: u32) u32 {
 
     var count: u32 = 0;
     for (containers, 0..) |container, i| {
-        count += countCombinations(containers[i + 1 ..], capacity + container);
+        count += countCombinations(containers[i + 1 ..], capacity + container, used + 1, limit);
     }
     return count;
+}
+
+fn minUsed(containers: []u32, capacity: u32, used: u32) u32 {
+    if (capacity == 150) {
+        return used;
+    } else if (capacity > 150) {
+        return std.math.maxInt(u32);
+    } else if (containers.len == 0) {
+        return std.math.maxInt(u32);
+    }
+
+    var min_used: u32 = std.math.maxInt(u32);
+    for (containers, 0..) |container, i| {
+        min_used = @min(min_used, minUsed(containers[i + 1 ..], capacity + container, used + 1));
+    }
+    return min_used;
 }

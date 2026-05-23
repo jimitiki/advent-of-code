5d316db2a730fc9641ac1bd7836e15f88da3bf91
jimitiki <dev@jimitiki.com>
2026-05-22 23:05:08 -0400

    2015.17: Implement part 1

--- solutions/2015/day17/src/main.zig
+++ solutions/2015/day17/src/main.zig
@@ -11,11 +11,30 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
+
+    var containers: std.ArrayList(u32) = .empty;
+    defer containers.deinit(bp.arena);
+
     while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+        try containers.append(bp.arena, try std.fmt.parseUnsigned(u32, line, 10));
     }
 
-    try stdout.print("{}\n", .{answer});
+    try stdout.print("{}\n", .{countCombinations(containers.items, 0)});
     try stdout.flush();
+}
+
+fn countCombinations(containers: []u32, capacity: u32) u32 {
+    if (capacity == 150) {
+        return 1;
+    } else if (capacity > 150) {
+        return 0;
+    } else if (containers.len == 0) {
+        return 0;
+    }
+
+    var count: u32 = 0;
+    for (containers, 0..) |container, i| {
+        count += countCombinations(containers[i + 1 ..], capacity + container);
+    }
+    return count;
 }

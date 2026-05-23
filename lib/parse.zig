c0dd2cf779d82e7019d4ca6b5a0813fcfef9dd89
jimitiki <dev@jimitiki.com>
2026-05-23 15:59:15 -0400

    Implement reverse word iteration

--- lib/parse.zig
+++ lib/parse.zig
@@ -6,12 +6,21 @@ pub const WordIterator = struct {
     string: []const u8,
     index: usize = 0,
     omit_punctuation: bool = false,
+    reverse: bool = false,
 
     pub fn init(string: []const u8) Self {
         return .{ .string = string };
     }
 
+    pub fn initRev(string: []const u8) Self {
+        return .{ .string = string, .index = string.len - 1, .reverse = true };
+    }
+
     pub fn next(self: *Self) ?[]const u8 {
+        return if (self.reverse) self.nextRev() else self.nextFwd();
+    }
+
+    fn nextFwd(self: *Self) ?[]const u8 {
         while (self.skip()) : (self.index += 1) {}
         if (self.index >= self.string.len) {
             return null;
@@ -22,8 +31,19 @@ pub const WordIterator = struct {
         return self.string[start..self.index];
     }
 
+    fn nextRev(self: *Self) ?[]const u8 {
+        while (self.skip()) : (self.index -= 1) {}
+        if (self.index <= 0) {
+            return null;
+        }
+
+        const end = self.index + 1;
+        while (self.take()) : (self.index -= 1) {}
+        return self.string[self.index + 1 .. end];
+    }
+
     fn skip(self: *Self) bool {
-        if (self.index >= self.string.len) {
+        if (self.reverse and self.index == 0 or self.index >= self.string.len) {
             return false;
         }
         return switch (self.string[self.index]) {
@@ -34,7 +54,7 @@ pub const WordIterator = struct {
     }
 
     fn take(self: *Self) bool {
-        if (self.index >= self.string.len) {
+        if (self.reverse and self.index == 0 or self.index >= self.string.len) {
             return false;
         }
         return switch (self.string[self.index]) {

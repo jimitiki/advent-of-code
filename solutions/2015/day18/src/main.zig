3d431894a6572bf9c23a9d7e49ae6c617988b74a
jimitiki <dev@jimitiki.com>
2026-05-22 23:57:36 -0400

    2015.18: Implement part 1

--- solutions/2015/day18/src/main.zig
+++ solutions/2015/day18/src/main.zig
@@ -3,6 +3,8 @@ const std = @import("std");
 const lib = @import("lib");
 const Boilerplate = lib.Boilerplate;
 
+const Row = std.bit_set.ArrayBitSet(usize, 102);
+
 pub fn main(init: std.process.Init) !void {
     var stdout_buffer: [256]u8 = undefined;
     var read_buffer: [256]u8 = undefined;
@@ -11,11 +13,54 @@ pub fn main(init: std.process.Init) !void {
 
     var stdout = &bp.stdout_writer.interface;
     var input = &bp.input_reader.interface;
-    const answer = 0;
-    while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
+
+    var grid_init = [_]Row{.empty} ** 102;
+    const grid_next = [_]Row{.empty} ** 102;
+    var i: usize = 1;
+    while (try input.takeDelimiter('\n')) |line| : (i += 1) {
+        grid_init[i] = parseRow(line);
     }
 
+    var grids: [2][102]Row = .{ grid_init, grid_next };
+    var cur: usize = 0;
+    for (0..100) |_| {
+        updateGrid(&grids[cur], &grids[(cur + 1) % 2]);
+        cur = (cur + 1) % 2;
+    }
+
+    var answer: usize = 0;
+    for (grids[cur]) |row| {
+        answer += row.count();
+    }
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
+}
+
+fn parseRow(string: []const u8) Row {
+    var row: Row = .empty;
+    for (string, 1..) |c, i| {
+        if (c == '#') {
+            row.set(i);
+        }
+    }
+    return row;
+}
+
+fn updateGrid(cur: []const Row, next: []Row) void {
+    for (cur[1 .. cur.len - 1], 1..cur.len - 1) |row, i| {
+        for (1..row.capacity() - 1) |j| {
+            var on_adj: u8 = 0;
+            for (i - 1..i + 2) |k| {
+                for (j - 1..j + 2) |l| {
+                    if (k == i and l == j) continue;
+                    if (cur[k].isSet(l)) on_adj += 1;
+                }
+            }
+            if (row.isSet(j)) {
+                next[i].setValue(j, on_adj == 2 or on_adj == 3);
+            } else {
+                next[i].setValue(j, on_adj == 3);
+            }
+        }
+    }
 }

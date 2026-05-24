ec0472686023357997268cb59bb622a16b61b87b
jimitiki <dev@jimitiki.com>
2026-05-24 00:43:34 -0400

    2015.22: Implement part 2

--- solutions/2015/day22/src/main.zig
+++ solutions/2015/day22/src/main.zig
@@ -25,7 +25,8 @@ pub fn main(init: std.process.Init) !void {
     const boss_hp = try parseBossStat(try input.takeDelimiter('\n'));
     const boss_atk = try parseBossStat(try input.takeDelimiter('\n'));
 
-    const answer = minMana(boss_atk, boss_hp, .{ .hp = 50, .mana = 500 }, 0, std.math.maxInt(u32));
+    const hp: u32 = if (bp.part == .p2) 49 else 50;
+    const answer = minMana(boss_atk, boss_hp, .{ .hp = hp, .mana = 500 }, 0, bp.part == .p2, 0);
     var stdout = &bp.stdout_writer.interface;
     try stdout.print("{}\n", .{answer});
     try stdout.flush();
@@ -43,22 +44,38 @@ fn parseBossStat(input: ?[]const u8) !u32 {
     }
 }
 
-fn minMana(boss_atk: u32, boss_hp: u32, wizard: Wizard, mana_spent: u32, min_spent: u32) u32 {
+var min_spent: u32 = std.math.maxInt(u32);
+
+fn minMana(boss_atk: u32, boss_hp: u32, wizard: Wizard, mana_spent: u32, hard_mode: bool, depth: usize) u32 {
     if (mana_spent >= min_spent) return std.math.maxInt(u32);
     if (boss_hp == 0) {
+        min_spent = mana_spent;
         return mana_spent;
     }
-    var min: u32 = min_spent;
+    var min: u32 = std.math.maxInt(u32);
     for (spells) |spell| {
-        const hp_new, const wizard_new, const cost = doTurn(boss_atk, boss_hp, wizard, spell) orelse continue;
-        min = @min(min, minMana(boss_atk, hp_new, wizard_new, mana_spent + cost, min));
+        const hp_new, const wizard_new, const cost = doTurn(boss_atk, boss_hp, wizard, spell, hard_mode) orelse continue;
+        const m = minMana(boss_atk, hp_new, wizard_new, mana_spent + cost, hard_mode, depth + 1);
+        min = @min(min, m);
     }
     return min;
 }
 
-fn doTurn(attack: u32, boss_hp_init: u32, wizard_init: Wizard, spell: Spell) ?struct { u32, Wizard, u32 } {
+fn doTurn(attack: u32, boss_hp_init: u32, wizard_init: Wizard, spell: Spell, hard_mode: bool) ?struct { u32, Wizard, u32 } {
     var boss_hp = boss_hp_init;
     var wizard = wizard_init;
+
+    // Wizard's turn
+    if (hard_mode) {
+        wizard.hp -|= 1;
+    }
+    if (wizard.hp == 0) {
+        return null;
+    }
+    applyEffects(&boss_hp, &wizard);
+    if (boss_hp == 0) {
+        return .{ boss_hp, wizard, 0 };
+    }
     const cost: u32, const spell_fn: *const fn (*u32, *Wizard) bool = switch (spell) {
         .magic_missile => .{ 53, magicMissile },
         .drain => .{ 73, drain },
@@ -68,15 +85,11 @@ fn doTurn(attack: u32, boss_hp_init: u32, wizard_init: Wizard, spell: Spell) ?st
     };
     if (cost > wizard.mana or !spell_fn(&boss_hp, &wizard)) return null;
     wizard.mana -= cost;
-    wizard.armor = 0;
+
+    // Boss' turn
     applyEffects(&boss_hp, &wizard);
     if (boss_hp > 0) {
         wizard.hp -|= @max(1, attack -| wizard.armor);
-        if (wizard.hp == 0) {
-            return null;
-        } else {
-            applyEffects(&boss_hp, &wizard);
-        }
     }
     return .{ boss_hp, wizard, cost };
 }
@@ -85,6 +98,8 @@ fn applyEffects(boss_hp: *u32, wizard: *Wizard) void {
     if (wizard.shield > 0) {
         wizard.armor = 7;
         wizard.shield -= 1;
+    } else {
+        wizard.armor = 0;
     }
     if (wizard.poison > 0) {
         boss_hp.* -|= 3;

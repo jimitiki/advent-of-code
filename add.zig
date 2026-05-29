c6bb39b26795063b957d6c0043b8c916cd690ebe
jimitiki <dev@jimitiki.com>
2026-05-29 19:55:48 -0400

    Rework script to add new solutions

--- add.zig
+++ add.zig
@@ -1,17 +1,51 @@
 const std = @import("std");
 
-const Solution = struct {
-    const Self = @This();
-    year: u16,
-    day: u8,
+const last_year = 25;
 
-    pub fn lessThan(_: void, lhs: Self, rhs: Self) bool {
-        if (lhs.year > rhs.year) {
-            return false;
-        } else if (lhs.year < rhs.year) {
-            return true;
-        } else {
-            return lhs.day < rhs.day;
+const YearIterator = struct {
+    reader: *std.Io.Reader,
+    index: usize = 0,
+
+    const Self = @This();
+
+    pub fn next(self: *Self) error{Malformed}!?u8 {
+        _ = self.reader.discardDelimiterInclusive('\n') catch unreachable;
+        while (true) {
+            if (self.reader.peekDelimiterInclusive('\n')) |line| {
+                if (extractYear(line)) |year| {
+                    return year;
+                } else if (std.mem.eql(u8, line, "};\n")) {
+                    return null;
+                }
+                _ = self.reader.discard(.limited(line.len)) catch unreachable;
+            } else |_| {
+                return error.Malformed;
+            }
+        }
+    }
+};
+
+const DayIterator = struct {
+    reader: *std.Io.Reader,
+    index: usize = 0,
+
+    const Self = @This();
+
+    pub fn next(self: *Self) error{Malformed}!?u8 {
+        _ = self.reader.discardDelimiterInclusive('\n') catch unreachable;
+        while (true) {
+            if (self.reader.peekDelimiterInclusive('\n')) |line| {
+                if (extractDay(line)) |day| {
+                    return day;
+                } else if (std.mem.eql(u8, line, "    },\n")) {
+                    return null;
+                } else if (std.mem.eql(u8, line, "    &.{},\n")) {
+                    return null;
+                }
+                _ = self.reader.discard(.limited(line.len)) catch unreachable;
+            } else |_| {
+                return error.Malformed;
+            }
         }
     }
 };
@@ -19,48 +53,177 @@ const Solution = struct {
 pub fn main(init: std.process.Init) !void {
     const gpa = init.arena.allocator();
     const args = try init.minimal.args.toSlice(gpa);
-    const year = 2000 + try std.fmt.parseInt(u16, args[2], 10);
-    const day = try std.fmt.parseInt(u8, args[3], 10);
 
     const dir = try std.Io.Dir.openDirAbsolute(init.io, args[1], .{});
-    // std.debug.print("{s}\n", .{path});
-    const zon = try dir.readFileAllocOptions(init.io, "solutions/solutions.zon", gpa, .unlimited, .@"1", 0);
-    const known_solutions = try std.zon.parse.fromSliceAlloc([]Solution, gpa, zon, null, .{});
-    var solutions = std.ArrayList(Solution).fromOwnedSlice(known_solutions);
-    for (solutions.items) |s| {
-        if (s.year == year and s.day == year) {
-            std.debug.print("{} day {} solution already exists", .{ year, day });
+    const year_arg: ?u8 = if (args.len < 3) null else try std.fmt.parseInt(u8, args[2], 10);
+    const day_arg: ?u8 = if (args.len < 4) null else try std.fmt.parseInt(u8, args[3], 10);
+    if (year_arg) |year| {
+        if (year < 15) {
+            std.debug.print("{} is before the first year", .{@as(u16, year) + 2000});
+            return;
+        }
+        if (year > last_year) {
+            std.debug.print("{} is beyond the last known year", .{@as(u16, year) + 2000});
+            return;
+        }
+        if (day_arg) |day| {
+            if (year >= 25 and day > 12 or day > 25) {
+                std.debug.print("{} is beyond the final date", .{day});
+                return;
+            }
+            if (day < 1) {
+                std.debug.print("{} is before the first date", .{day});
+                return;
+            }
+        }
+    }
+
+    const text = try dir.readFileAlloc(init.io, "src/solutions.zig", gpa, .unlimited);
+    var reader = std.Io.Reader.fixed(text);
+    const year, const day, const exists = findOpenDayAndYear(year_arg, day_arg, &reader) catch |err| {
+        switch (err) {
+            error.AlreadyExists => std.debug.print("Solution already exists\n", .{}),
+            error.Malformed => std.debug.print("Solutions file is malformed\n", .{}),
+            error.YearFull => std.debug.print("No missing solutions for year\n", .{}),
+        }
+        return;
+    };
+
+    const solutions_file = try dir.createFile(init.io, "src/solutions.zig", .{ .truncate = true });
+    defer solutions_file.close(init.io);
+    var buf: [1024]u8 = undefined;
+    var file_writer = solutions_file.writer(init.io, &buf);
+    var writer = &file_writer.interface;
+
+    _ = try writer.write(text[0..reader.seek]);
+    const expand_year = std.mem.eql(u8, "    &.{},\n", try reader.peekDelimiterInclusive('\n'));
+    if (expand_year) _ = try reader.discardDelimiterInclusive('\n');
+    if (!exists or expand_year) {
+        if (!exists) {
+            try writer.print("    // 20{}\n", .{year});
+        }
+        _ = try writer.write("    &.{\n");
+    }
+    try writer.print("        @import(\"y{}/d{:0>2}.zig\").solve,\n", .{ year, day });
+    if (!exists or expand_year) {
+        _ = try writer.write("    },\n");
+    }
+    _ = try writer.write(text[reader.seek..]);
+    try writer.flush();
+
+    var dir_buf: [16]u8 = undefined;
+    var file_buf: [16]u8 = undefined;
+    const input_dir_path = try std.fmt.bufPrint(&dir_buf, "inputs/y{}/", .{year});
+    const input_file_name = try std.fmt.bufPrint(&file_buf, "d{:0>2}.txt", .{day});
+    const input_dir = try dir.createDirPathOpen(init.io, input_dir_path, .{});
+    if (input_dir.createFile(init.io, input_file_name, .{ .exclusive = true })) |_| {} else |err| {
+        switch (err) {
+            error.PathAlreadyExists => {},
+            else => |e| return e,
+        }
+    }
+
+    const src_dir_path = try std.fmt.bufPrint(&dir_buf, "src/y{}/", .{year});
+    const src_file_name = try std.fmt.bufPrint(&file_buf, "d{:0>2}.zig", .{day});
+    const src_dir = try dir.createDirPathOpen(init.io, src_dir_path, .{});
+    if (std.Io.Dir.copyFile(dir, "template.zig", src_dir, src_file_name, init.io, .{ .replace = false, .make_path = true })) |_| {} else |err| {
+        switch (err) {
+            error.PathAlreadyExists => {},
+            else => |e| return e,
+        }
+    }
+}
+
+fn findOpenDayAndYear(year: ?u8, day: ?u8, reader: *std.Io.Reader) error{ AlreadyExists, YearFull, Malformed }!struct { u8, u8, bool } {
+    if (day) |d| {
+        const exists = try advanceToYear(year.?, reader);
+        _ = try advanceToDay(d, reader);
+        return .{ year.?, d, exists };
+    }
+
+    if (year) |y| {
+        const exists = try advanceToYear(y, reader);
+        if (!exists) {
+            return .{ @intCast(y), 1, true };
+        }
+        const d = try findOpenDay(y, reader);
+        return .{ y, d, true };
+    }
+
+    for (15..last_year) |y| {
+        const yr: u8 = @intCast(y);
+        const exists = advanceToYear(yr, reader) catch break;
+        if (!exists) {
+            return .{ yr, 1, true };
+        }
+        if (findOpenDay(yr, reader)) |d| {
+            return .{ yr, d, true };
+        } else |err| {
+            switch (err) {
+                error.YearFull => {},
+                error.Malformed => return err,
+            }
+        }
+    }
+    return error.AlreadyExists;
+}
+
+fn findOpenDay(year: u8, reader: *std.Io.Reader) error{ YearFull, Malformed }!u8 {
+    var day: u8 = 0;
+    var it: DayIterator = .{ .reader = reader };
+    while (try it.next()) |d| {
+        if (d - day > 1) {
+            return day + 1;
+        }
+        day = d;
+    }
+    if (day >= 25 or year >= 25 and day >= 12) {
+        return error.YearFull;
+    } else {
+        return day + 1;
+    }
+}
+
+/// Advances the reader's position to the first line that declares a solution in the provided year.
+/// Returns `true` if the year already exists in the file, or `false` otherwise.
+fn advanceToYear(year: u8, reader: *std.Io.Reader) error{Malformed}!bool {
+    var it: YearIterator = .{ .reader = reader };
+    while (try it.next()) |y| {
+        if (y == year) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/// Advances the reader's position to the line where the provided day's solution should go. Assumes
+/// that the reader has been advanced to the correct year
+fn advanceToDay(day: u8, reader: *std.Io.Reader) error{ AlreadyExists, Malformed }!void {
+    var it: DayIterator = .{ .reader = reader };
+    while (try it.next()) |d| {
+        if (d == day) {
+            return error.AlreadyExists;
+        } else if (d > day) {
             return;
         }
     }
-    try solutions.append(gpa, .{ .day = day, .year = year });
-    std.sort.pdq(Solution, solutions.items, {}, Solution.lessThan);
+    return;
+}
 
-    const subpath = try std.fmt.allocPrint(gpa, "solutions/{}/day{}", .{ year, day });
-    const subdir = try dir.createDirPathOpen(init.io, subpath, .{});
-    try subdir.createDirPath(init.io, "data");
-    if (std.Io.Dir.copyFile(dir, "template.zig", subdir, "src/main.zig", init.io, .{ .replace = false, .make_path = true })) |_| {} else |err| {
-        switch (err) {
-            error.PathAlreadyExists => {},
-            else => |e| return e,
-        }
-    }
-    if (subdir.createFile(init.io, "data/real.txt", .{ .exclusive = true })) |_| {} else |err| {
-        switch (err) {
-            error.PathAlreadyExists => {},
-            else => |e| return e,
-        }
-    }
-    if (subdir.createFile(init.io, "data/test.txt", .{ .exclusive = true })) |_| {} else |err| {
-        switch (err) {
-            error.PathAlreadyExists => {},
-            else => |e| return e,
-        }
-    }
+fn extractYear(line: []const u8) ?u8 {
+    const prefix = "    // 20";
+    if (!startsWith(prefix, line)) return null;
+    return std.fmt.parseInt(u8, line[prefix.len .. prefix.len + 2], 10) catch null;
+}
 
-    var buf: [1024]u8 = undefined;
-    var solutions_file = try dir.openFile(init.io, "solutions/solutions.zon", .{ .mode = .write_only });
-    var writer = solutions_file.writer(init.io, &buf);
-    try std.zon.stringify.serialize(solutions.items, .{}, &writer.interface);
-    try writer.flush();
+fn extractDay(line: []const u8) ?u8 {
+    const prefix = "        @import(\"y";
+    if (!startsWith(prefix, line)) return null;
+    return std.fmt.parseInt(u8, line[prefix.len + 4 .. prefix.len + 6], 10) catch return null;
+}
+
+fn startsWith(prefix: []const u8, str: []const u8) bool {
+    if (str.len < prefix.len) return false;
+    if (!std.mem.eql(u8, str[0..prefix.len], prefix)) return false;
+    return true;
 }
--- template.zig
+++ template.zig
@@ -1,21 +1,11 @@
 const std = @import("std");
 
-const lib = @import("lib");
-const Boilerplate = lib.Boilerplate;
+const solver = @import("../solver.zig");
 
-pub fn main(init: std.process.Init) !void {
-    var stdout_buffer: [256]u8 = undefined;
-    var read_buffer: [256]u8 = undefined;
-    var bp = try Boilerplate.init(init, &stdout_buffer, &read_buffer);
-    defer bp.deinit();
-
-    var stdout = &bp.stdout_writer.interface;
-    var input = &bp.input_reader.interface;
-    const answer = 0;
-    while (try input.takeDelimiter('\n')) |line| {
-        _ = line;
-    }
-
-    try stdout.print("{}\n", .{answer});
-    try stdout.flush();
+fn solveInt(gpa: std.mem.Allocator, input: *std.Io.Reader) solver.Error!struct { ?u32, ?u32 } {
+    _ = gpa;
+    _ = input;
+    return .{ null, null };
 }
+
+pub const solve = solver.intSolver(u32, solveInt);

27c2e8272e270f445e33370a208298c3e8f9dba7
jimitiki <dev@jimitiki.com>
2026-06-01 08:04:16 -0400

    2016.10: Add outputs

--- src/y16/d10.zig
+++ src/y16/d10.zig
@@ -8,25 +8,33 @@ const Source = union(enum) {
     input: u16,
     bot: u16,
 };
+const Destination = union(enum) {
+    output: u16,
+    bot: u16,
+};
 const Bot = struct {
     id: u16,
     source_a: ?Source = null,
     source_b: ?Source = null,
     value_low: ?u16 = null,
     value_high: ?u16 = null,
-    output_low: ?u16 = null,
-    output_high: ?u16 = null,
+    dest_low: ?Destination = null,
+    dest_high: ?Destination = null,
 };
 const Inputs = std.AutoHashMapUnmanaged(u16, u16);
+const Outputs = std.AutoHashMapUnmanaged(u16, u16);
 const Bots = std.AutoHashMapUnmanaged(u16, Bot);
 
 fn solveInt(gpa: Allocator, input: *std.Io.Reader) solver.Error!struct { ?u32, ?u32 } {
     var inputs: Inputs = .empty;
-    var bots: Bots = .empty;
     defer inputs.deinit(gpa);
+    var outputs: Outputs = .empty;
+    defer outputs.deinit(gpa);
+    var bots: Bots = .empty;
     defer bots.deinit(gpa);
+
     while (try input.takeDelimiter('\n')) |instruction| {
-        try parseInstruction(gpa, &inputs, &bots, instruction);
+        try parseInstruction(gpa, &inputs, &outputs, &bots, instruction);
     }
     return .{ try findIntersection(inputs, &bots, 61, 17), null };
 }
@@ -34,6 +42,7 @@ fn solveInt(gpa: Allocator, input: *std.Io.Reader) solver.Error!struct { ?u32, ?
 fn parseInstruction(
     gpa: Allocator,
     inputs: *Inputs,
+    outputs: *Outputs,
     bots: *Bots,
     instruction: []const u8,
 ) solver.Error!void {
@@ -43,37 +52,44 @@ fn parseInstruction(
         const value = try parseInt(it.next().?);
         for (0..3) |_| _ = it.next();
         const bot_id = try parseInt(it.next().?);
-        try addSource(gpa, inputs, bots, bot_id, .{ .input = value });
+        try addSource(gpa, bots, bot_id, .{ .input = value });
+        try inputs.putNoClobber(gpa, value, bot_id);
     } else if (std.mem.eql(u8, kind, "bot")) {
         const bot_id = try parseInt(it.next().?);
         for (0..3) |_| _ = it.next();
         const low = try parseOutput(it.next().?, it.next().?);
         for (0..3) |_| _ = it.next();
         const high = try parseOutput(it.next().?, it.next().?);
-
-        const result = try bots.getOrPut(gpa, bot_id);
-        const bot_ptr = result.value_ptr;
-        if (!result.found_existing) {
-            bot_ptr.* = .{ .id = bot_id };
-        }
-        bot_ptr.output_low = low;
-        bot_ptr.output_high = high;
-        if (low) |dest_bot_id| {
-            try addSource(gpa, inputs, bots, dest_bot_id, .{ .bot = bot_id });
-        }
-        if (high) |dest_bot_id| {
-            try addSource(gpa, inputs, bots, dest_bot_id, .{ .bot = bot_id });
-        }
+        try addBot(gpa, outputs, bots, bot_id, low, high);
     } else {
         return error.InvalidInput;
     }
 }
 
-fn addSource(gpa: Allocator, inputs: *Inputs, bots: *Bots, bot_id: u16, source: Source) error{OutOfMemory}!void {
+fn addBot(
+    gpa: Allocator,
+    outputs: *Outputs,
+    bots: *Bots,
+    bot_id: u16,
+    dest_low: Destination,
+    dest_high: Destination,
+) solver.Error!void {
     const result = try bots.getOrPut(gpa, bot_id);
     const bot_ptr = result.value_ptr;
     if (!result.found_existing) {
         bot_ptr.* = .{ .id = bot_id };
+    }
+    bot_ptr.dest_low = dest_low;
+    bot_ptr.dest_high = dest_high;
+    try addDestination(gpa, outputs, bots, bot_id, dest_low);
+    try addDestination(gpa, outputs, bots, bot_id, dest_high);
+}
+
+fn addSource(gpa: Allocator, bots: *Bots, dest_bot_id: u16, source: Source) error{OutOfMemory}!void {
+    const result = try bots.getOrPut(gpa, dest_bot_id);
+    const bot_ptr = result.value_ptr;
+    if (!result.found_existing) {
+        bot_ptr.* = .{ .id = dest_bot_id };
     }
     if (bot_ptr.source_b) |_| {
         unreachable;
@@ -82,17 +98,23 @@ fn addSource(gpa: Allocator, inputs: *Inputs, bots: *Bots, bot_id: u16, source: 
     } else {
         bot_ptr.source_a = source;
     }
-    switch (source) {
-        .input => |value| try inputs.putNoClobber(gpa, value, bot_id),
-        else => {},
+}
+
+fn addDestination(gpa: Allocator, outputs: *Outputs, bots: *Bots, bot_id: u16, destination: Destination) error{OutOfMemory}!void {
+    switch (destination) {
+        .output => |output| try outputs.putNoClobber(gpa, output, bot_id),
+        .bot => |dest_bot_id| try addSource(gpa, bots, dest_bot_id, .{ .bot = bot_id }),
     }
 }
 
-fn parseOutput(kind: []const u8, id: []const u8) error{InvalidInput}!?u16 {
+fn parseOutput(kind: []const u8, id: []const u8) error{InvalidInput}!Destination {
+    const out_id = try parseInt(id);
     if (std.mem.eql(u8, kind, "bot")) {
-        return try parseInt(id);
+        return .{ .bot = out_id };
+    } else if (std.mem.eql(u8, kind, "output")) {
+        return .{ .output = out_id };
     } else {
-        return null;
+        return error.InvalidInput;
     }
 }
 
@@ -102,34 +124,34 @@ fn parseInt(str: []const u8) error{InvalidInput}!u16 {
 
 fn findIntersection(inputs: Inputs, bots: *Bots, value_high: u16, value_low: u16) solver.Error!u16 {
     var bot = bots.getPtr(inputs.get(value_high).?).?;
-    try computeValues(bots, bot);
+    try computeBotValues(bots, bot);
     var i: usize = 0;
     while (bot.value_high.? != value_high or bot.value_low.? != value_low) : (i += 1) {
         if (value_high == bot.value_high) {
-            bot = bots.getPtr(bot.output_high.?).?;
+            bot = bots.getPtr(bot.dest_high.?.bot).?;
         } else if (value_high == bot.value_low) {
-            bot = bots.getPtr(bot.output_low.?).?;
+            bot = bots.getPtr(bot.dest_low.?.bot).?;
         }
-        try computeValues(bots, bot);
+        try computeBotValues(bots, bot);
     }
     return bot.id;
 }
 
-fn computeValues(bots: *Bots, bot: *Bot) solver.Error!void {
+fn computeBotValues(bots: *Bots, bot: *Bot) solver.Error!void {
     if (bot.value_high) |_| return;
-    const value_a = try computeValue(bots, bot.source_a.?, bot.id);
-    const value_b = try computeValue(bots, bot.source_b.?, bot.id);
+    const value_a = try computeBotValue(bots, bot.source_a.?, bot.id);
+    const value_b = try computeBotValue(bots, bot.source_b.?, bot.id);
     bot.value_high = @max(value_a, value_b);
     bot.value_low = @min(value_a, value_b);
 }
 
-fn computeValue(bots: *Bots, source: Source, bot_id: u16) solver.Error!u16 {
+fn computeBotValue(bots: *Bots, source: Source, bot_id: u16) solver.Error!u16 {
     switch (source) {
         .input => |value| return value,
         .bot => |src_bot_id| {
             const src_bot = bots.getPtr(src_bot_id).?;
-            try computeValues(bots, src_bot);
-            if (src_bot.output_high.? == bot_id) {
+            try computeBotValues(bots, src_bot);
+            if (src_bot.dest_high.?.bot == bot_id) {
                 return src_bot.value_high.?;
             } else {
                 return src_bot.value_low.?;

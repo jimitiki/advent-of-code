2a801a3d4dd3299fffb3d48245b792e26df28b54
jimitiki <dev@jimitiki.com>
2026-06-03 23:23:01 -0400

    2016.14: Implement part 2

--- src/y16/d14.zig
+++ src/y16/d14.zig
@@ -1,19 +1,22 @@
 const std = @import("std");
 
 const solver = @import("../solver.zig");
-const hashIndex = @import("../hash.zig").hashIndex;
+const hash = @import("../hash.zig");
 
 const Candidate = struct { index: usize, char: u8 };
 const Queue = std.Deque(Candidate);
 
 fn solveInt(tools: solver.Tools) solver.Error!struct { ?usize, ?usize } {
     const salt = try tools.input.takeDelimiter('\n') orelse return error.InvalidInput;
-    return .{ try computeIndex(tools.gpa, salt, 64), null };
+    return .{
+        try computeIndex(tools.gpa, salt, 64, 0),
+        try computeIndex(tools.gpa, salt, 64, 2016),
+    };
 }
 
 pub const solve = solver.intSolver(usize, solveInt);
 
-fn computeIndex(gpa: std.mem.Allocator, salt: []const u8, key_cnt: usize) error{OutOfMemory}!?usize {
+fn computeIndex(gpa: std.mem.Allocator, salt: []const u8, key_cnt: usize, hash_stretches: usize) error{OutOfMemory}!?usize {
     var candidates: Queue = .empty;
     defer candidates.deinit(gpa);
     var quintuples = [_]usize{0} ** 16;
@@ -23,7 +26,7 @@ fn computeIndex(gpa: std.mem.Allocator, salt: []const u8, key_cnt: usize) error{
     var count: usize = 0;
     var index: usize = 0;
     while (count < key_cnt) : (index += 1) {
-        hashIndex(salt, index, &buf, &hex) catch @panic("Buffer is not big enough");
+        stretchHash(salt, index, &buf, &hex, hash_stretches);
 
         if (checkThree(&hex)) |char| {
             try candidates.pushBack(gpa, .{
@@ -52,7 +55,7 @@ fn computeIndex(gpa: std.mem.Allocator, salt: []const u8, key_cnt: usize) error{
 }
 
 test "compute index" {
-    try std.testing.expectEqual(22728, try computeIndex(std.testing.allocator, "abc", 64));
+    try std.testing.expectEqual(22728, try computeIndex(std.testing.allocator, "abc", 64, 0));
 }
 
 fn checkThree(hex: *[32]u8) ?u8 {
@@ -63,4 +66,24 @@ fn checkThree(hex: *[32]u8) ?u8 {
         return char;
     }
     return null;
+}
+
+fn stretchHash(salt: []const u8, index: usize, buf: []u8, hex: *[32]u8, n: usize) void {
+    hash.hashIndex(salt, index, buf, hex) catch @panic("Buffer is too small");
+    for (0..n) |_| {
+        hash.hashStr(hex, hex);
+    }
+}
+
+test "hash stretching" {
+    var hex: [32]u8 = undefined;
+    var buf: [4]u8 = undefined;
+    stretchHash("abc", 0, &buf, &hex, 0);
+    try std.testing.expectEqualSlices(u8, "577571be4de9dcce85a041ba0410f29f", &hex);
+    stretchHash("abc", 0, &buf, &hex, 1);
+    try std.testing.expectEqualSlices(u8, "eec80a0c92dc8a0777c619d9bb51e910", &hex);
+    stretchHash("abc", 0, &buf, &hex, 2);
+    try std.testing.expectEqualSlices(u8, "16062ce768787384c81fe17a7a60c7e3", &hex);
+    stretchHash("abc", 0, &buf, &hex, 2016);
+    try std.testing.expectEqualSlices(u8, "a107ff634856bb300138cac6568c0f24", &hex);
 }

29f401df73c53ffa3035147bf1e3ecc62628b41d
jimitiki <dev@jimitiki.com>
2026-06-07 13:17:29 -0400

    2016.24: Implement part 2

--- src/y16/d24.zig
+++ src/y16/d24.zig
@@ -11,7 +11,6 @@ fn Maze(comptime n: u8) type {
     return struct {
         const Self = @This();
         const BitSetInt = @Int(.unsigned, n);
-        const PoiInt = std.math.Log2Int(BitSetInt);
         const wire_count = n;
         const goal = std.math.maxInt(BitSetInt);
 
@@ -24,27 +23,21 @@ fn Maze(comptime n: u8) type {
             }
         };
 
-        layout: []const []const ?PoiInt,
-        rowlist: std.ArrayList([]const ?PoiInt),
+        layout: [][]u8,
+        rowlist: std.ArrayList([]u8),
 
-        pub fn parse(gpa: std.mem.Allocator, reader: *std.Io.Reader) solver.Error!struct { Self, State } {
+        pub fn parse(gpa: std.mem.Allocator, reader: *std.Io.Reader) solver.Error!struct { Self, Position } {
             const width = (reader.peekDelimiterExclusive('\n') catch return error.InvalidInput).len;
-            var rows: std.ArrayList([]const ?PoiInt) = .empty;
-            var start: State = undefined;
+            var rows: std.ArrayList([]u8) = .empty;
+            var start: Position = undefined;
             var y: usize = 0;
             while (try reader.takeDelimiter('\n')) |line| : (y += 1) {
                 if (line.len != width) return error.InvalidInput;
-                var row = try gpa.alloc(?PoiInt, width);
+                var row = try gpa.alloc(u8, width);
                 for (line, 0..) |char, x| {
-                    switch (char) {
-                        '1', '2', '3', '4', '5', '6', '7' => row[x] = @intCast(char - 48),
-                        '0' => {
-                            start = .{ .pos = .{ .x = x, .y = y } };
-                            row[x] = 0;
-                        },
-                        '.' => row[x] = 0,
-                        '#' => row[x] = null,
-                        else => return error.InvalidInput,
+                    row[x] = char;
+                    if (char == '0') {
+                        start = .{ .x = x, .y = y };
                     }
                 }
                 try rows.append(gpa, row);
@@ -58,15 +51,16 @@ fn Maze(comptime n: u8) type {
             self.rowlist.deinit(gpa);
         }
 
-        fn shortestPath(self: Self, gpa: std.mem.Allocator, start: State) error{OutOfMemory}!?u16 {
+        fn shortestPath(self: Self, gpa: std.mem.Allocator, start: Position) error{OutOfMemory}!?u16 {
             var queue: std.PriorityQueue(struct { u16, State }, void, cmp) = .empty;
             defer queue.deinit(gpa);
             var minima: std.AutoHashMapUnmanaged(State, u16) = .empty;
             defer minima.deinit(gpa);
             var state_buf: [4]State = undefined;
 
-            try queue.push(gpa, .{ 0, start });
-            try minima.put(gpa, start, 0);
+            const initial: State = .{ .pos = start, .visited = 0 };
+            try queue.push(gpa, .{ 0, initial });
+            try minima.put(gpa, initial, 0);
 
             while (queue.pop()) |entry| {
                 const dist, const state = entry;
@@ -110,8 +104,16 @@ fn Maze(comptime n: u8) type {
         }
 
         fn newStateIfValid(self: Self, state: State, x: usize, y: usize) ?State {
-            const poi = self.layout[y][x] orelse return null;
-            const visited = if (poi > 0) state.visited | @shlExact(@as(BitSetInt, 1), poi - 1) else state.visited;
+            const tile = self.layout[y][x];
+            if (tile == '#') {
+                return null;
+            }
+            var visited = state.visited;
+            if (tile >= '1' and tile <= '7') {
+                visited |= std.math.shl(BitSetInt, 1, tile - 49);
+            } else if (tile == '8' and visited == goal >> 1) {
+                visited = goal;
+            }
             return .{ .pos = .{ .x = x, .y = y }, .visited = visited };
         }
 
@@ -122,9 +124,14 @@ fn Maze(comptime n: u8) type {
 }
 
 fn solveInt(tools: solver.Tools) solver.Error!struct { ?u16, ?u16 } {
-    var maze, const start = try Maze(7).parse(tools.gpa, tools.input);
-    defer maze.deinit(tools.gpa);
-    return .{ try maze.shortestPath(tools.gpa, start), null };
+    var maze1, const start = try Maze(7).parse(tools.gpa, tools.input);
+    defer maze1.deinit(tools.gpa);
+    const p1 = try maze1.shortestPath(tools.gpa, start);
+
+    var maze2: Maze(8) = .{ .layout = maze1.layout, .rowlist = maze1.rowlist };
+    maze2.layout[start.y][start.x] = '8';
+
+    return .{ p1, try maze2.shortestPath(tools.gpa, start) };
 }
 
 pub const solve = solver.intSolver(u16, solveInt);

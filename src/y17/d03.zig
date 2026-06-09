a3a3b539a204302ec066d45662370991e8e10a5c
jimitiki <dev@jimitiki.com>
2026-06-09 09:10:20 -0400

    2017.3: Implement part 2

--- src/y17/d03.zig
+++ src/y17/d03.zig
@@ -2,11 +2,29 @@ const std = @import("std");
 
 const solver = @import("../solver.zig");
 
+const Position = struct { i16, i16 };
+const Grid = std.AutoHashMapUnmanaged(Position, u32);
+const Dir = enum {
+    down,
+    left,
+    right,
+    up,
+
+    fn next(self: Dir) Dir {
+        return switch (self) {
+            .down => .right,
+            .left => .down,
+            .right => .up,
+            .up => .left,
+        };
+    }
+};
+
 fn solveInt(tools: solver.Tools) solver.Error!struct { ?u32, ?u32 } {
     const line = try tools.input.takeDelimiter('\n') orelse return error.InvalidInput;
     const input = std.fmt.parseUnsigned(u32, line, 10) catch return error.InvalidInput;
 
-    return .{ distance(input), null };
+    return .{ distance(input), try findLarger(tools.gpa, input) };
 }
 
 pub const solve = solver.intSolver(u32, solveInt);
@@ -43,3 +61,59 @@ test "distance" {
     try std.testing.expectEqual(31, distance(1024));
 }
 
+fn findLarger(gpa: std.mem.Allocator, target: u32) error{OutOfMemory}!u32 {
+    var grid: Grid = .empty;
+    defer grid.deinit(gpa);
+
+    var position: Position = .{ 0, 0 };
+    var ring: u32 = 0;
+    var edge_size: u32 = 0;
+    var edge_end: u32 = 2;
+    var dir: Dir = .right;
+    var index: u32 = 1;
+    while (try computeValue(gpa, &grid, position) <= target) {
+        index += 1;
+        switch (dir) {
+            .down => position[1] += 1,
+            .left => position[0] -= 1,
+            .right => position[0] += 1,
+            .up => position[1] -= 1,
+        }
+        if (index == edge_end) {
+            dir = dir.next();
+            if (dir == .right) {
+                edge_end += edge_size + 1;
+            } else if (dir == .up) {
+                ring += 1;
+                edge_size = ring * 2;
+                edge_end = edge_end + edge_size - 1;
+            } else {
+                edge_end += edge_size;
+            }
+        }
+    }
+
+    return grid.get(position).?;
+}
+
+fn computeValue(gpa: std.mem.Allocator, grid: *Grid, position: Position) error{OutOfMemory}!u32 {
+    var sum: u32 = 0;
+    for (0..3) |i| {
+        const x = position[0] + @as(i8, @intCast(i)) - 1;
+        for (0..3) |j| {
+            const y = position[1] + @as(i8, @intCast(j)) - 1;
+            if (x == position[0] and y == position[1]) continue;
+            sum += grid.get(.{ x, y }) orelse 0;
+        }
+    }
+    sum = @max(1, sum);
+    try grid.put(gpa, position, sum);
+    return sum;
+}
+
+test "find larger" {
+    const gpa = std.testing.allocator;
+    try std.testing.expectEqual(23, try findLarger(gpa, 22));
+    try std.testing.expectEqual(330, try findLarger(gpa, 329));
+    try std.testing.expectEqual(351, try findLarger(gpa, 330));
+}

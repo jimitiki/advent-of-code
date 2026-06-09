afd154b39a77e62f1311fde99ef947d10273c8d7
jimitiki <dev@jimitiki.com>
2026-06-09 11:59:42 -0400

    2017.5: Implement part 2

--- src/y17/d05.zig
+++ src/y17/d05.zig
@@ -8,14 +8,21 @@ fn solveInt(tools: solver.Tools) solver.Error!struct { ?u32, ?u32 } {
     while (try tools.input.takeDelimiter('\n')) |line| {
         try offsets.append(tools.gpa, std.fmt.parseInt(i16, line, 10) catch return error.InvalidInput);
     }
+    const copy = try tools.gpa.alloc(i16, offsets.items.len);
+    defer tools.gpa.free(copy);
+    @memcpy(copy, offsets.items);
+    return .{ countJumps(offsets.items, false), countJumps(copy, true) };
+}
+
+fn countJumps(offsets: []i16, dec_three: bool) u32 {
     var pc: u32 = 0;
     var jumps: u32 = 0;
-    while (pc < offsets.items.len) : (jumps += 1) {
-        const offset = offsets.items[pc];
-        offsets.items[pc] += 1;
+    while (pc < offsets.len) : (jumps += 1) {
+        const offset = offsets[pc];
+        offsets[pc] += if (dec_three and offset >= 3) -1 else 1;
         pc = @intCast(@as(i32, @intCast(pc)) + offset);
     }
-    return .{ jumps, null };
+    return jumps;
 }
 
 pub const solve = solver.intSolver(u32, solveInt);

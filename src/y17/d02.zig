ad92c151cc5361bac6c93f1f24a084259b477415
jimitiki <dev@jimitiki.com>
2026-06-08 22:17:27 -0400

    2017.2: Implement part 2

--- src/y17/d02.zig
+++ src/y17/d02.zig
@@ -4,15 +4,24 @@ const solver = @import("../solver.zig");
 const Parser = @import("../Parser.zig");
 
 fn solveInt(tools: solver.Tools) solver.Error!struct { ?u16, ?u16 } {
+    var row: std.ArrayList(u16) = .empty;
+    defer row.deinit(tools.gpa);
+
     var chksum: u16 = 0;
-    while (try tools.input.takeDelimiter('\n')) |line| {
+    var divsum: u16 = 0;
+    while (try tools.input.takeDelimiter('\n')) |line| : (row.clearRetainingCapacity()) {
         var parser: Parser = .init(line, .{});
         var min: u16 = std.math.maxInt(u16);
         var max: u16 = 0;
         while (true) {
             if (parser.takeInt(u16)) |n| {
+                for (row.items) |m| {
+                    if (std.math.divExact(u16, n, m)) |q| divsum += q else |_| {}
+                    if (std.math.divExact(u16, m, n)) |q| divsum += q else |_| {}
+                }
                 min = @min(min, n);
                 max = @max(max, n);
+                try row.append(tools.gpa, n);
             } else |err| {
                 switch (err) {
                     error.EndOfBuffer => break,
@@ -22,7 +31,7 @@ fn solveInt(tools: solver.Tools) solver.Error!struct { ?u16, ?u16 } {
         }
         chksum += max - min;
     }
-    return .{ chksum, null };
+    return .{ chksum, divsum };
 }
 
 pub const solve = solver.intSolver(u16, solveInt);

5f7af1ab8dabb35bce24dea2127f90a5c33d30e6
jimitiki <dev@jimitiki.com>
2026-06-12 15:53:11 -0400

    2017.17: Implement part 2

--- src/y17/d17.zig
+++ src/y17/d17.zig
@@ -3,18 +3,20 @@ const std = @import("std");
 const solver = @import("../solver.zig");
 const t = @import("../test.zig");
 
+// TODO: Implement part 1 without shifting memory at every step.
+
 const Spinlock = struct {
-    buf: *[2018]u16,
-    step: u16,
+    buf: *[2018]u64,
+    step: u64,
     size: usize = 1,
     index: usize = 0,
 
-    pub fn init(buf: *[2018]u16, step: u16) Spinlock {
+    pub fn init(buf: *[2018]u64, step: u64) Spinlock {
         buf[0] = 0;
         return .{ .buf = buf, .step = step };
     }
 
-    pub fn insert(self: *Spinlock, item: u16) void {
+    pub fn insert(self: *Spinlock, item: u64) void {
         self.index = self.bufIndex(self.index + self.step) + 1;
         @memmove(self.buf[self.index + 1 .. self.size + 1], self.buf[self.index..self.size]);
         self.buf[self.index] = item;
@@ -26,20 +28,29 @@ const Spinlock = struct {
     }
 };
 
-fn solveInt(tools: solver.Tools) solver.Error!struct { ?u16, ?u16 } {
+fn solveInt(tools: solver.Tools) solver.Error!struct { ?u64, ?u64 } {
     const input = try tools.input.takeDelimiter('\n') orelse return error.InvalidInput;
-    const step = std.fmt.parseInt(u16, input, 10) catch return error.InvalidInput;
-    var buf: [2018]u16 = undefined;
+    const step = std.fmt.parseInt(u64, input, 10) catch return error.InvalidInput;
+    var buf: [2018]u64 = undefined;
     var spinlock: Spinlock = .init(&buf, step);
 
     for (1..2018) |i| {
         spinlock.insert(@intCast(i));
     }
-    return .{ spinlock.buf[spinlock.bufIndex(spinlock.index + 1)], null };
+
+    var index: usize = 0;
+    var p2: u64 = 0;
+    for (1..50_000_000) |i| {
+        index = (index + step) % i + 1;
+        if (index == 1) {
+            p2 = i;
+        }
+    }
+    return .{ spinlock.buf[spinlock.bufIndex(spinlock.index + 1)], p2 };
 }
 
-pub const solve = solver.intSolver(u16, solveInt);
+pub const solve = solver.intSolver(u64, solveInt);
 
 test "solve" {
-    try t.expectIntSolution(u16, solveInt, .{ 638, null }, "3");
+    try t.expectIntSolution(u64, solveInt, .{ 638, 1222153 }, "3");
 }

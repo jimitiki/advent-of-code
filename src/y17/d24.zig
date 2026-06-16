aac2c41fba50b4c82fa5c9e6d8548d65db18425a
jimitiki <dev@jimitiki.com>
2026-06-16 19:47:03 -0400

    2017.24: Preallocate for set of used components

--- src/y17/d24.zig
+++ src/y17/d24.zig
@@ -24,51 +24,50 @@ fn solveInt(tools: solver.Tools) solver.Error!struct { ?u16, ?u16 } {
     }
     var used: ComponentSet = .empty;
     defer used.deinit(tools.gpa);
+    try used.ensureTotalCapacity(tools.gpa, @intCast(component_list.items.len));
     return .{
-        try strongest(tools.gpa, component_list.items, &used, 0, 0),
-        (try longest(tools.gpa, component_list.items, &used, 0, 0))[1],
+        strongest(component_list.items, &used, 0, 0),
+        longest(component_list.items, &used, 0, 0)[1],
     };
 }
 
 pub const solve = solver.intSolver(u16, solveInt);
 
 fn strongest(
-    gpa: std.mem.Allocator,
     components: []const Component,
     used: *ComponentSet,
     port: u8,
     strength: u16,
-) error{OutOfMemory}!u16 {
-    var max: u16 = strength;
+) u16 {
+    var max = strength;
     for (components) |component| {
         if (used.contains(component)) continue;
 
         const next_port: ?u8 = if (component[0] == port) component[1] else if (component[1] == port) component[0] else null;
         if (next_port) |p| {
-            try used.put(gpa, component, {});
+            used.putAssumeCapacity(component, {});
             defer _ = used.remove(component);
-            max = @max(max, try strongest(gpa, components, used, p, strength + component[0] + component[1]));
+            max = @max(max, strongest(components, used, p, strength + component[0] + component[1]));
         }
     }
     return max;
 }
 
 fn longest(
-    gpa: std.mem.Allocator,
     components: []const Component,
     used: *ComponentSet,
     port: u8,
     strength: u16,
-) error{OutOfMemory}!struct { u16, u16 } {
+) struct { u16, u16 } {
     var max: struct { u16, u16 } = .{ @intCast(used.size), strength };
     for (components) |component| {
         if (used.contains(component)) continue;
 
         const next_port: ?u8 = if (component[0] == port) component[1] else if (component[1] == port) component[0] else null;
         if (next_port) |p| {
-            try used.put(gpa, component, {});
+            used.putAssumeCapacity(component, {});
             defer _ = used.remove(component);
-            const len, const str = try longest(gpa, components, used, p, strength + component[0] + component[1]);
+            const len, const str = longest(components, used, p, strength + component[0] + component[1]);
             if (len > max[0] or len == max[0] and str > max[1]) {
                 max = .{ len, str };
             }

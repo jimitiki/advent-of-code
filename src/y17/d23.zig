946177b03dd47e62e454266e02bde7f8ee6a65ba
jimitiki <dev@jimitiki.com>
2026-06-16 11:22:01 -0400

    2017.23: Implement part 2

--- src/y17/d23.zig
+++ src/y17/d23.zig
@@ -8,7 +8,21 @@ fn solveInt(tools: solver.Tools) solver.Error!struct { ?u32, ?u32 } {
     var parser: Parser = .init(line, .{});
     try parser.skipMany(2);
     const b = try parser.takeInt(u16);
-    return .{ (b - 2) * (b - 2), null };
+
+    var h: u32 = 0;
+    var i: u32 = 100000 + 100 * @as(u32, b);
+    const end: u32 = i + 17000;
+    while (i <= end) : (i += 17) {
+        if (is_prime(i)) h += 1;
+    }
+    return .{ (b - 2) * (b - 2), h };
 }
 
 pub const solve = solver.intSolver(u32, solveInt);
+
+fn is_prime(n: u32) bool {
+    for (2..std.math.sqrt(n) + 1) |i| {
+        if (n % i == 0) return false;
+    }
+    return true;
+}

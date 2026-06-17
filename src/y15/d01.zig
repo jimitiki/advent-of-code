00071d3e2d1b49c18d36f17eef763900d000525a
jimitiki <dev@jimitiki.com>
2026-06-17 00:02:25 -0400

    2015.1: Rewrite to use entire input text

--- src/y15/d01.zig
+++ src/y15/d01.zig
@@ -4,18 +4,16 @@ const solver = @import("../solver.zig");
 fn solveInt(input: solver.Input, tools: solver.Tools) error{InvalidInput}!struct { ?i32, ?i32 } {
     _ = tools;
     var floor: i32 = 0;
-    var pos: i32 = 1;
     var answer: ?i32 = null;
-    while (true) : (pos += 1) {
-        const c = input.reader.takeByte() catch break;
-        switch (c) {
+    for (input.text, 0..) |char, i| {
+        switch (char) {
             '(' => floor += 1,
             ')' => floor -= 1,
             '\n' => break,
             else => return error.InvalidInput,
         }
         if (floor < 0) {
-            answer = answer orelse pos;
+            answer = answer orelse @intCast(i + 1);
         }
     }
     return .{ floor, answer };

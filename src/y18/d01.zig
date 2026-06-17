53c1c65d16d4c3f407c1e6d89e37a858cbd73074
jimitiki <dev@jimitiki.com>
2026-06-17 00:37:59 -0400

    2018.1: Implement part 2

--- src/y18/d01.zig
+++ src/y18/d01.zig
@@ -4,19 +4,33 @@ const solver = @import("../solver.zig");
 const testing = @import("../testing.zig");
 
 fn solveInt(input: solver.Input, tools: solver.Tools) solver.Error!struct { ?i32, ?i32 } {
-    _ = tools;
+    var change_list: std.ArrayList(i32) = .empty;
+    defer change_list.deinit(tools.gpa);
+
     var frequency: i32 = 0;
     while (try input.parser.parseInt(i32)) |n| {
+        try change_list.append(tools.gpa, n);
         frequency += n;
     }
-    return .{ frequency, null };
+
+    var frequencies: std.AutoHashMapUnmanaged(i32, void) = .empty;
+    defer frequencies.deinit(tools.gpa);
+
+    var f: i32 = 0;
+    var i: usize = 0;
+    while (true) : (i = (i + 1) % change_list.items.len) {
+        const change = change_list.items[i];
+        f += change;
+        const result = try frequencies.getOrPut(tools.gpa, f);
+        if (result.found_existing) break;
+    } else unreachable;
+
+    return .{ frequency, f };
 }
 
 pub const solve = solver.intSolver(i32, solveInt);
 
 test "solve" {
-    try testing.expectIntSolution(i32, solveInt, .{ 3, null }, "+1\n-2\n+3\n+1");
-    try testing.expectIntSolution(i32, solveInt, .{ 3, null }, "+1\n+1\n+1");
-    try testing.expectIntSolution(i32, solveInt, .{ 0, null }, "+1\n+1\n-2");
-    try testing.expectIntSolution(i32, solveInt, .{ -6, null }, "-1\n-2\n-3");
+    try testing.expectIntSolution(i32, solveInt, .{ 3, 2 }, "+1\n-2\n+3\n+1");
+    try testing.expectIntSolution(i32, solveInt, .{ 0, 1 }, "+1\n+1\n-2");
 }

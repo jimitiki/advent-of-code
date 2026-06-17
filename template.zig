a349d0b293654a009c05cf95c16efff6b1611409
jimitiki <dev@jimitiki.com>
2026-06-17 18:22:09 -0400

    Add other input handling options to template

--- template.zig
+++ template.zig
@@ -5,10 +5,17 @@ const testing = @import("../testing.zig");
 
 fn solveInt(input: solver.Input, tools: solver.Tools) solver.Error!struct { ?u32, ?u32 } {
     _ = tools;
-    var reader = input.reader();
-    while (try reader.takeDelimiter('\n')) |line| {
+
+    _ = input.firstLine();
+
+    var parser = input.parser(.{});
+    _ = parser.takeInt(u32);
+
+    var lines = input.lines();
+    while (lines.next()) |line| {
         _ = line;
     }
+
     return .{ null, null };
 }
 

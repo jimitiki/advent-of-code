99459b7289b3587d68e5ac21edd8ef1833d2e035
jimitiki <dev@jimitiki.com>
2026-06-18 09:21:34 -0400

    zig/18-05: Implement part 2

--- zig/src/y18/d05.zig
+++ zig/src/y18/d05.zig
@@ -4,21 +4,40 @@ const solver = @import("../solver.zig");
 const testing = @import("../testing.zig");
 
 fn solveInt(input: solver.Input, tools: solver.Tools) solver.Error!struct { ?usize, ?usize } {
-    var stack: std.ArrayList(u8) = .empty;
-    defer stack.deinit(tools.gpa);
-    for (try input.firstLine()) |char| {
-        if (stack.items.len > 0 and stack.items[stack.items.len - 1] == char ^ 0b00100000) {
-            _ = stack.pop();
-        } else {
-            try stack.append(tools.gpa, char);
-        }
+    const gpa = tools.gpa;
+    var first_reduction: std.ArrayList(u8) = .empty;
+    defer first_reduction.deinit(gpa);
+
+    try reduce(gpa, try input.firstLine(), &first_reduction, 0);
+
+    var second_reduction: std.ArrayList(u8) = .empty;
+    defer second_reduction.deinit(gpa);
+
+    var min_size: usize = std.math.maxInt(usize);
+    for ('a'..'z') |unit| {
+        try reduce(gpa, first_reduction.items, &second_reduction, @intCast(unit));
+        min_size = @min(min_size, second_reduction.items.len);
     }
-    return .{ stack.items.len, null };
+
+    return .{ first_reduction.items.len, min_size };
 }
 
 pub const solve = solver.intSolver(usize, solveInt);
 
 test "solve" {
     const input = "dabAcCaCBAcCcaDA";
-    try testing.expectIntSolution(usize, solveInt, .{ 10, null }, input);
+    try testing.expectIntSolution(usize, solveInt, .{ 10, 4 }, input);
+}
+
+fn reduce(gpa: std.mem.Allocator, polymer: []const u8, stack: *std.ArrayList(u8), unit: u8) error{OutOfMemory}!void {
+    stack.clearRetainingCapacity();
+    for (polymer) |char| {
+        if (char == unit or char ^ 0b00100000 == unit) continue;
+
+        if (stack.items.len > 0 and stack.items[stack.items.len - 1] == char ^ 0b00100000) {
+            _ = stack.pop();
+        } else {
+            try stack.append(gpa, char);
+        }
+    }
 }

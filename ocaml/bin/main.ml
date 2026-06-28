28c8d43777ae88920b6296c5fde544f54ee571a0
jimitiki <dev@jimitiki.com>
2026-06-28 09:47:27 -0400

    ocaml: Report elapsed time

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -16,13 +16,28 @@ let get_arg i name =
 let year = get_arg 1 "year"
 let day = get_arg 2 "day"
 
+let run_solver (module S : Solver) input =
+  let t0 = Sys.time () in
+  let res =
+    match S.solve input with
+    | exception Bad_input s -> Error s
+    | p1, p2 -> Ok (p1, p2)
+  in
+  let elapsed = Sys.time () -. t0 in
+  (elapsed, res)
+
 let () =
   match Solvers.Registry.get (year, day) with
   | None -> printf "No solution found for year %d day %d\n" (year + 2000) day
-  | Some (module S : Solver) -> (
+  | Some (module S : Solver) ->
       printf "\n[%d.%d]:\n" (year + 2000) day;
-      match S.solve (Input.read year day) with
-      | exception Bad_input s -> printf "  Error: %s\n" s
-      | p1, p2 ->
-          printf "  Part 1: %s\n  Part 2: %s\n" (Ans.to_string p1)
-            (Ans.to_string p2))
+      let input = Input.read year day in
+      let elapsed, res = run_solver (module S) input in
+      let () =
+        match res with
+        | Error s -> printf "  Error: %s\n" s
+        | Ok (p1, p2) ->
+            let p1, p2 = (Ans.to_string p1, Ans.to_string p2) in
+            printf "  Part 1: %s\n  Part 2: %s\n" p1 p2
+      in
+      printf "  [Elapsed = %.6f]\n" elapsed
